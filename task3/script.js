d5d30285a3a38adb38033e7c1395e9bc56319c27
Litta <111124374+Gravelytta@users.noreply.github.com>
2022-12-17 18:51:36 +0400

    Add files via upload

---
+++ task3/script.js
@@ -0,0 +1,45 @@
+const trafficLightEl = document.querySelector('#trafficLight1');
+const trafficLightEl2 = document.querySelector('#trafficLight2');
+const trafficLightEl3 = document.querySelector('#trafficLight3');
+
+trafficLightEl.addEventListener('click', makeGreen);
+trafficLightEl2.addEventListener('click', makeGreen);
+trafficLightEl3.addEventListener('click', makeGreen);
+
+function makeGreen() {
+    trafficLightEl.style.background = ('green');
+    trafficLightEl3.style.background = ('black');
+    trafficLightEl.removeEventListener('click', makeGreen);
+    trafficLightEl2.removeEventListener('click', makeGreen);
+    trafficLightEl3.removeEventListener('click', makeGreen);
+    trafficLightEl.addEventListener('click', makeYellow);
+    trafficLightEl2.addEventListener('click', makeYellow);
+    trafficLightEl3.addEventListener('click', makeYellow);
+}
+
+
+function makeYellow() {
+    trafficLightEl.style.background = ('black');
+    trafficLightEl2.style.background = ('yellow');
+    trafficLightEl.removeEventListener('click', makeYellow);
+    trafficLightEl2.removeEventListener('click', makeYellow);
+    trafficLightEl3.removeEventListener('click', makeYellow);
+    trafficLightEl.addEventListener('click', makeRed);
+    trafficLightEl2.addEventListener('click', makeRed);
+    trafficLightEl3.addEventListener('click', makeRed);
+}
+
+
+function makeRed() {
+    trafficLightEl2.style.background = ('black');
+    trafficLightEl3.style.background = ('red');
+    trafficLightEl.removeEventListener('click', makeRed);
+    trafficLightEl2.removeEventListener('click', makeRed);
+    trafficLightEl3.removeEventListener('click', makeRed);
+    trafficLightEl.addEventListener('click', makeGreen);
+    trafficLightEl2.addEventListener('click', makeGreen);
+    trafficLightEl3.addEventListener('click', makeGreen);
+}
+
+trafficLightEl.addEventListener('click', makeGreen);
+
